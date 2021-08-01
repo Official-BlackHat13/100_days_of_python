0592e99c1d32344353e3aabd845bfb1c912340e2
huba <huba0ferencz@gmail.com>
2021-08-01 21:47:19 +0300

    final

--- day_2.py
+++ day_2.py
@@ -1,9 +1,9 @@
-two_digit_number = input('enter a two digit number: ')
+print('welcome to the tim calculator!')
+bill = input('bill: ')
+tip_percentage = input('percentage of the tip you want to give: ')
+persons = input('number of people who eated: ')
 
-first_num = int(two_digit_number[0])
-second_num = int(two_digit_number[1])
-sum_of_nums = first_num + second_num
-
-final = str(first_num) + ' + ' + str(second_num) + ' = ' + str(sum_of_nums)
-
-print(final)
+tip = (float(bill) * float(tip_percentage)) / 100
+total_bill = (float(bill) + float(tip))
+bill_per_person = float(total_bill) / float(persons)
+print('each person should pay ' + str(bill_per_person))
