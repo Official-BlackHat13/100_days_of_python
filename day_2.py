c50435f0d61bd02e7b893e09a438a60c84318255
huba <huba0ferencz@gmail.com>
2021-08-01 19:10:18 +0300

    test

---
+++ day_2.py
@@ -0,0 +1,9 @@
+two_digit_number = input('enter a two digit number: ')
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+sum_of_nums = first_num + second_num
+
+final = str(first_num) + ' + ' + str(second_num) + ' = ' + str(sum_of_nums)
+
+print(final)
