66022826d79168724baea29679927260e56be156
huba <huba0ferencz@gmail.com>
2021-09-05 11:54:04 +0300

    final

--- day_11_blackjack/day_11.py
+++ day_11_blackjack/day_11.py
@@ -1,9 +1,10 @@
+from day_11_1 import *
 from random import randint
-
+print(blackjack_logo)
 # black jack
 list_of_cards = [
     # ACES:
-    # [1, 11], [1, 11], [1, 11], [1, 11],
+    [1, 11], [1, 11], [1, 11], [1, 11],
     # COMMONS
     2, 2, 2, 2,
     3, 3, 3, 3,
@@ -26,26 +27,77 @@ comp_cards = []
 user_cards = []
 comp_total = sum(comp_cards)
 user_total = sum(user_cards)
-len_of_list = len(list_of_cards)
+
+
+
+
 def game():
+    global comp_cards
+    global user_cards
+    global comp_total
+    global user_total
+
     should_continue = input('type \'y\' to get another card or \'n\' to pass: ')
 
     def choosen_card():
         global list_of_cards
-        random_card = randint(0, len_of_list - 1)
-        print(random_card)
-        card = list_of_cards[random_card]
-        print(card)
-        list_of_cards.remove(list_of_cards[random_card])
+        random_card = randint(0, len(list_of_cards) - 1)
+
+        list_of_cards.remove(list_of_cards[random_card - 1])
+
+        card = list_of_cards[random_card - 1]
+
+
         if card == [1, 11]:
-            print('hello')
+            if comp_total + 11 > 21:
+                card = 1
+            else:
+                card = 11
+            if user_total + 11 > 21:
+                card = 1
+            else:
+                card = 11
         return card
 
     if should_continue == 'y':
         comp_cards.append(choosen_card())
         user_cards.append(choosen_card())
-
-        print(f'computer\'s first card: {comp_cards}\nyour card(s): {user_cards}, current score: {sum(user_cards)}')
+        comp_total = sum(comp_cards)
+        user_total = sum(user_cards)
+        if comp_total == 21:
+            print(f'\nBLACKJACK! computer won with the score of {comp_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if user_total == 21:
+            print(f'\nBLACKJACK! you won with the score of {user_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if len(comp_cards) >= 5 and comp_total < 21:
+            print(f'\nCHARLIE! computer has the score of {comp_total}, but got {len(comp_cards)}, The computer won\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if len(comp_cards) >= 5 and comp_total < 21:
+            print(f'\nCHARLIE! computer has the score of {comp_total}, but got {len(comp_cards)}, The computer won\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if comp_total > 21 and user_total < 21:
+            print(f'\nyou won with the score of {user_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if user_total > 21 and comp_total < 21:
+            print(f'\ncomputer won with the score of {comp_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        if comp_total >= 21 and comp_total == user_total:
+            print(f'\ndraw\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+            return
+        print(f'computer\'s first card: {comp_cards}\nyour card(s): {user_cards}, current score: {user_total}')
         game()
+
+    if should_continue == 'n':
+        comp_total = sum(comp_cards)
+        user_total = sum(user_cards)
+        if comp_total < 21 and comp_total > user_total:
+            print(f'\ncomputer won with the score of {comp_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+        if user_total < 21 and user_total > comp_total:
+            print(f'\nyou won with the score of {user_total}.\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+        if comp_total <= 21 and comp_total == user_total:
+            print(f'\ndraw\ncomputer\'s cards: {comp_cards}\nyour cards:{user_cards}')
+
+
 
 game()
