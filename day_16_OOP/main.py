862ba71649c8c71863cb0a232ca627de9bfcdef7
huba <huba0ferencz@gmail.com>
2021-09-15 06:13:22 +0300

    final

--- day_16_OOP/main.py
+++ day_16_OOP/main.py
@@ -1,16 +1,3 @@
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape('turtle')
-# timmy.color('chartreuse4')
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-
-table = PrettyTable()
-print(table)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
