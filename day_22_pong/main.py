968b89633e24c2fc032b8335a907c2d60163eea4
huba <huba0ferencz@gmail.com>
2021-09-28 18:57:35 +0300

    final

--- day_22_pong/main.py
+++ day_22_pong/main.py
@@ -1,9 +1,25 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Snake')
+screen.tracer(0)
 
+r_paddle = Paddle((370, 0))
+l_paddle = Paddle((-370, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 's')
+screen.onkey(l_paddle.go_down, 'x')
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
+
