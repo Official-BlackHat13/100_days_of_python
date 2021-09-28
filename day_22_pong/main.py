5bbeafc1f35c85e364d2ed476bede57d5905e09f
huba <huba0ferencz@gmail.com>
2021-09-28 18:18:40 +0300

    final

--- day_22_pong/main.py
+++ day_22_pong/main.py
@@ -1,23 +1,9 @@
-from turtle import Turtle, Screen
-from equipment import Player1, Player2
-from middle_line import Line
-import time
-
-player1 = Player1()
-player2 = Player2()
-line = Line()
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=800, height=800)
+screen.setup(width=800, height=600)
 screen.bgcolor('black')
-screen.tracer(0)
+screen.title('Snake')
 
-screen.listen()
-
-screen.onkey(player1.move_up, 'Up')
-screen.onkey(player1.move_down, 'Down')
-
-screen.onkey(player2.up, 'w')
-screen.onkey(player2.down, 's')
 
 screen.exitonclick()
