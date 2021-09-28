af9214997ae7cbf821576e91419cad9579d32d85
huba <huba0ferencz@gmail.com>
2021-09-28 19:55:52 +0300

    final

--- day_22_pong/main.py
+++ day_22_pong/main.py
@@ -1,14 +1,17 @@
 from turtle import Screen, Turtle
+import time
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
-screen.title('Snake')
+screen.title('Pong')
 screen.tracer(0)
 
 r_paddle = Paddle((370, 0))
 l_paddle = Paddle((-370, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, 'Up')
@@ -16,10 +19,14 @@ screen.onkey(r_paddle.go_down, 'Down')
 screen.onkey(l_paddle.go_up, 's')
 screen.onkey(l_paddle.go_down, 'x')
 
-
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() == 280 or ball.ycor() == -280:
+        ball.bounce()
 
 screen.exitonclick()
-
