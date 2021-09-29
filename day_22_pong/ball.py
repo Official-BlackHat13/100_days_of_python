d56adc322e57f5f89c72de7684c13b164b32cc2f
huba <huba0ferencz@gmail.com>
2021-09-29 08:21:09 +0300

    final

--- day_22_pong/ball.py
+++ day_22_pong/ball.py
@@ -17,7 +17,15 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-        print(self.position())
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset(self):
+        self.hideturtle()
+        self.goto(0, 0)
+        self.showturtle()
+        self.x_move *= -1
--- day_22_pong/paddle.py
+++ day_22_pong/paddle.py
@@ -13,7 +13,9 @@ class Paddle(Turtle):
     def go_up(self):
         new_ycor = self.ycor() + 20
         self.goto(self.xcor(), new_ycor)
+        print(self.position())
 
     def go_down(self):
         new_ycor = self.ycor() - 20
         self.goto(self.xcor(), new_ycor)
+        print(self.position())
