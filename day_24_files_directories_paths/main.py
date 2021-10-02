855acc8572e8d53755cd870632f99f5eb4c17d8b
huba <huba0ferencz@gmail.com>
2021-10-02 06:00:29 +0300

    final

--- day_24_files_directories_paths/main.py
+++ day_24_files_directories_paths/main.py
@@ -1,3 +1,13 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+# with open("my_file.txt", mode="r") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("my_file.txt", mode="w") as file:
+#     file.write("new Text.")
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("new Text.")
+#
+
+with open("new_file.txt", mode="w") as file:
+    file.write("new Text.")
