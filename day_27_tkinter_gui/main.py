a192567aab23e84a95f9607e3091e0e67b45c261
huba <huba0ferencz@gmail.com>
2021-10-11 06:40:43 +0300

    final

--- day_27_tkinter_gui/main.py
+++ day_27_tkinter_gui/main.py
@@ -1,12 +1,29 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title('Mi to Km App')
 window.minsize(height=300, width=400)
 
-my_label = tkinter.Label(text='Miles to Kilometers App', font=('Arial', 20, 'normal'))
+# create texts
+my_label = Label(text='Miles to Kilometers App', font=('Arial', 20, 'normal'))
 my_label.pack()
 
+
+# my_label['text'] = 'New Text'
+# my_label.config(text='text')
+
+# create buttons
+def button_clicked():
+    my_label['text'] = calc_input.get()
+
+
+button = Button(text='click me', command=button_clicked)
+button.pack()
+
+# create input fields
+calc_input = Entry(width=10)
+calc_input.pack()
+calc_input.get()
 
 window.mainloop()
