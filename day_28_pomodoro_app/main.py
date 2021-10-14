12cc1bc36937dca861689d3176bfadd98bfaa953
huba <huba0ferencz@gmail.com>
2021-10-14 16:55:23 +0300

    final

--- day_28_pomodoro_app/main.py
+++ day_28_pomodoro_app/main.py
@@ -2,17 +2,17 @@ from tkinter import *
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#FF6D3A"
+ORANGE = "#FF6D3A"
 RED = "#FF4C4C"
-GREEN = "#3D6687"
-YELLOW = "#f7f5dd"
+BLUE = "#3D6687"
+GREEN = "#E1FFF0"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 message = 'timer'
-color = GREEN
+color = BLUE
 timer = None
 
 
@@ -49,10 +49,10 @@ def start_timer():
         title_label.config(text='break', fg=RED)
         long_break_min()
     elif reps % 2 == 0:
-        title_label.config(text='break', fg=PINK)
+        title_label.config(text='break', fg=ORANGE)
         short_break_min()
     else:
-        title_label.config(text='work', fg=GREEN)
+        title_label.config(text='work', fg=BLUE)
         work_min()
 
 
@@ -71,7 +71,6 @@ def count_down(count):
         timer = window.after(1000, count_down, count - 1)
     else:
         marks = ''
-        start_timer()
         work_session = math.floor(reps / 2)
         for _ in range(work_session):
             marks += '✔'
@@ -83,34 +82,34 @@ def count_down(count):
 # creating window
 window = Tk()
 window.title('Pomodoro App')
-window.config(padx=50, pady=25, bg=YELLOW)
+window.config(padx=50, pady=25, bg=GREEN)
 window.maxsize(400, 400)
 
 # adding app title
-title_label = Label(text=message, fg=color, bg=YELLOW, font=(FONT_NAME, 50))
+title_label = Label(text=message, fg=color, bg=GREEN, font=(FONT_NAME, 45, 'bold'))
 title_label.grid(column=1, row=0)
 
 # adding image as canvas
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=GREEN, highlightthickness=0)
 tomato_img = PhotoImage(file='stopwatch.png')
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text='00:00', fill='#0D5177', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='#3D6687', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
 # creating buttons
-start_button_img = PhotoImage(file='start.png')
-start_button_img_label = Label(image=start_button_img)
 
-start_button = Button(image=start_button_img, bg=YELLOW, command=start_timer, highlightthickness=0, borderwidth=0)
+# start button
+start_button_img = PhotoImage(file='start.png')
+start_button = Button(image=start_button_img, bg=GREEN, command=start_timer, highlightthickness=0, borderwidth=0)
 start_button.grid(column=0, row=2)
 
+# reset button
 reset_button_img = PhotoImage(file='stop-button.png')
-reset_button_img_label = Label(image=reset_button_img)
-
-reset_button = Button(image=reset_button_img, bg=YELLOW, command=reset_timer, highlightthickness=0, borderwidth=0)
+reset_button = Button(image=reset_button_img, bg=GREEN, command=reset_timer, highlightthickness=0, borderwidth=0)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(fg=GREEN, bg=YELLOW)
+# adding checkmarks
+check_marks = Label(fg=BLUE, bg=GREEN)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
