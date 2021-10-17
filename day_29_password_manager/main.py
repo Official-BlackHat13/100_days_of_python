aa477124be8f0ea41fe74207bf42b42526878f55
huba <huba0ferencz@gmail.com>
2021-10-17 11:48:00 +0300

    final

--- day_29_password_manager/main.py
+++ day_29_password_manager/main.py
@@ -2,9 +2,21 @@ from tkinter import *
 
 FONT_NAME = "Courier"
 
+
 # ----------------------- PASSWORD GENERATOR ----------------------- #
 
 # ----------------------- SAVE PASSWORD ----------------------- #
+
+def save():
+    website = website_input.get()
+    username = username_input.get()
+    password = password_input.get()
+    with open('data.txt', 'a') as data_file:
+        data_file.write(f"{website} | {username} | {password}\n")
+        website_input.delete(0, END)
+        username_input.delete(0, END)
+        password_input.delete(0, END)
+
 
 # ----------------------- UI SETUP ----------------------- #
 
@@ -33,7 +45,7 @@ website_label.grid(row=1, column=0)
 # input
 website_input = Entry(width=35)
 website_input.grid(row=1, column=1, columnspan=2)
-
+website_input.focus()
 
 # -- email/username field -- #
 
@@ -44,7 +56,7 @@ username_label.grid(row=2, column=0)
 # input
 username_input = Entry(width=35)
 username_input.grid(row=2, column=1, columnspan=2)
-
+username_input.insert(0, 'huba0ferencz@gmail.com')
 
 # -- password field -- #
 
@@ -60,11 +72,10 @@ password_input.grid(row=3, column=1, columnspan=2)
 generate_button = Button(text='generate', bg='#f0f0f0')
 generate_button.grid(row=3, column=2, columnspan=2)
 
-
 # -- submit field -- #
 
 # button
 
-add_button = Button(text='add', width=32, bg='#f0f0f0')
+add_button = Button(text='add', width=32, bg='#f0f0f0', command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 window.mainloop()
