4fede16f2be66cec438eb20a4f1694460c92da4c
huba <huba0ferencz@gmail.com>
2021-10-21 19:50:29 +0300

    final

--- day_31_flash_card_app/main.py
+++ day_31_flash_card_app/main.py
@@ -13,9 +13,11 @@ to_learn = data.to_dict(orient='records')
 
 # ----------- BRAIN ----------- #
 
+
 def next_card():
     current_card = random.choice(to_learn)
-    canvas.itemconfig(english_text, title=current_card["Magyar"])
+    canvas.itemconfig(english_text, text=current_card['En'])
+    canvas.itemconfig(magyar_text, text=current_card['Hu'])
 
 
 # ----------- UI ----------- #
@@ -27,20 +29,23 @@ canvas = Canvas(width=800, height=800)
 flash_card = PhotoImage(file='media/flashcard.png')
 canvas.create_image(400, 400, image=flash_card)
 # english text
-english_text = canvas.create_text(400, 230, text=f'English', font=('Ariel', 40, 'italic'))
+english_text = canvas.create_text(400, 230, text=f'', font=('Ariel', 40, 'italic'))
 
 # magyar text
-magyar_text = canvas.create_text(400, 590, text=f'Magyar', font=('Ariel', 40, 'italic'))
+magyar_text = canvas.create_text(400, 590, text=f'', font=('Ariel', 40, 'italic'))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=1)
 
 cross_image = PhotoImage(file='media/wrong_button.png')
-unknown_button = Button(image=cross_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=next_card)
 unknown_button.grid(row=0, column=0)
 
 checkmark_image = PhotoImage(file='media/right_button.png')
-known_button = Button(image=checkmark_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=next_card)
+known_button = Button(image=checkmark_image, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0,
+                      command=next_card)
 known_button.grid(row=0, column=2)
+
+next_card()
 
 window.mainloop()
