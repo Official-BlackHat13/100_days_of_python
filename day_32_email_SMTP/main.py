697440efc812c10161c59c95550e9f6c6cc7ed76
huba <huba0ferencz@gmail.com>
2021-10-22 20:37:11 +0300

    final

--- day_32_email_SMTP/main.py
+++ day_32_email_SMTP/main.py
@@ -1,21 +1,32 @@
-import smtplib
+# import smtplib
+#
+# my_email = 'nyelrugosibakugan@gmail.com'
+# my_password = 'macskafasza'
+#
+#
+# # connection = smtplib.SMTP('smtp.gmail.com')
+# # connection.starttls()
+# # connection.login(user=my_email, password=my_password)
+# # connection.sendmail(from_addr=my_email,
+# #                     to_addrs='huba0ferencz@gmail.com',
+# #                     msg='Subject:szewasz bazmeg\n\nkutyaidat setaltatod?')
+# # connection.close()
+# # alternative below
+#
+# with smtplib.SMTP('smtp.gmail.com') as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs='huba0ferencz@gmail.com',
+#                         msg='Subject:szewasz bazmeg\n\nkutyaidat setaltatod?')
 
-my_email = 'nyelrugosibakugan@gmail.com'
-my_password = 'macskafasza'
+import datetime as dt
 
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
 
-# connection = smtplib.SMTP('smtp.gmail.com')
-# connection.starttls()
-# connection.login(user=my_email, password=my_password)
-# connection.sendmail(from_addr=my_email,
-#                     to_addrs='huba0ferencz@gmail.com',
-#                     msg='Subject:szewasz bazmeg\n\nkutyaidat setaltatod?')
-# connection.close()
-# alternative below
+date_of_birth = dt.datetime(year=2004, month=12, day=20)
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs='huba0ferencz@gmail.com',
-                        msg='Subject:szewasz bazmeg\n\nkutyaidat setaltatod?')
+print(date_of_birth)
